fb1cac36228b085d712774ea4ef8d4073f5159cc
Vitor Pacheco <vitor-p.c@hotmail.com>
2012-07-05 23:23:23 -0300

    Teste do plugin CakePdf.

---
+++ Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '099605',
+		'database' => 'cakephp_blog',
+		'encoding' => 'utf8'
+	);
+}
---
+++ View/Layouts/pdf/default.ctp
@@ -0,0 +1,3 @@
+<pre>
+	<?php echo $this->fetch('content');?>
+</pre>
